4097757e1946fccf10ab6b048d003141ad7b7948
McjMzn <maz.an@interia.pl>
2021-10-17 23:39:50 +0200

    added ReflectionRpcWebServer class

--- ReflectionRpc.Asp/Program.cs
+++ ReflectionRpc.Asp/Program.cs
@@ -1,26 +1,8 @@
 using ReflectionRpc.Asp;
-using ReflectionRpc.Core;
-using ReflectionRpc.Core.RpcResponses;
 using ReflectionRpc.Examples.Service;
 
-var builder = WebApplication.CreateBuilder(args);
+var server = new ReflectionRpcWebServer();
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+server.AddHostedService(new ConsoleLoggingService(), "Console");
 
-builder.Services.AddRazorPages();
-
-builder.Services.AddReflectionRpc();
-
-var app = builder.Build();
-
-app.UseSwagger();
-app.UseSwaggerUI();
-app.MapGet("/", () => Results.LocalRedirect("/swagger/index.html"));
-
-app.UseReflectionRpc();
-app.HostReflectionRpcService(new ConsoleLoggingService(), "Console");
-
-app.MapRazorPages();
-
-app.Run();
+server.Run("http://localhost:12350");
---
+++ ReflectionRpc.Asp/ReflectionRpcWebServer.cs
@@ -0,0 +1,39 @@
+﻿namespace ReflectionRpc.Asp
+{
+    public class ReflectionRpcWebServer
+    {
+        private WebApplication app;
+
+        public ReflectionRpcWebServer()
+        {
+            var builder = WebApplication.CreateBuilder(Environment.GetCommandLineArgs());
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddRazorPages();
+
+            builder.Services.AddReflectionRpc();
+            
+            this.app = builder.Build();
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.MapGet("/", () => Results.LocalRedirect("/rpc/ui/hosts"));
+
+            app.UseReflectionRpc();
+
+            app.MapRazorPages();
+        }
+
+        public void AddHostedService(object service, string tag)
+        {
+            this.app.HostReflectionRpcService(service, tag);
+        }
+
+        public void Run(string url = null)
+        {
+            this.app.Run(url);
+        }
+    }
+}
